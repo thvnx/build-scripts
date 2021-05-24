96c973fa4fee2fd74a8d6f2f3634e2030dc0df50
Laurent Thévenoux <lthevenoux@kalray.eu>
2021-05-24 09:59:04 +0200

    fixup! Build LLVM toolchain based on OSP offer

--- build-kvx-llvm.sh
+++ build-kvx-llvm.sh
@@ -3,15 +3,9 @@ set -eu
 
 ## Set these for using specific revision
 SHA1_LLVM=${SHA1_LLVM:-HEAD}
-SHA1_BINUTILS=${SHA1_BINUTILS:-HEAD}
-SHA1_NEWLIB=${SHA1_NEWLIB:-HEAD}
 
-TARGET=${TARGET:-kvx-elf}
-TRIPLE=kvx-kalray-osp
+TRIPLE=kvx-kalray-cos
 PREFIX=$(realpath "$1")
-
-PARALLEL_JOBS=-j6
-
 
 mkdir -p "$PREFIX"
 export PATH="$PREFIX/bin:$PATH"
@@ -48,26 +42,7 @@ function git_clone() {
     fi
 }
 
-git_clone https://github.com/kalray/gdb-binutils.git "${SHA1_BINUTILS}" -
-git_clone https://github.com/kalray/newlib.git "${SHA1_NEWLIB}" coolidge
 git_clone https://github.com/kalray/llvm-project "${SHA1_LLVM}" kalray/12.x/kvx-12.0.0
-
-mkdir -p build-binutils
-pushd build-binutils
-../gdb-binutils/configure \
-    --prefix="$PREFIX" \
-    --target="$TARGET" \
-    --disable-initfini-array  \
-    --disable-gdb \
-    --without-gdb \
-    --disable-werror   \
-    --with-expat=yes \
-    --with-babeltrace=no \
-    --with-bugurl=no
-
-make all "$PARALLEL_JOBS" > /dev/null
-make install
-popd
 
 mkdir -p build-llvm
 pushd build-llvm
@@ -77,37 +52,9 @@ cmake -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_EXAMPLES=False \
       -DLLVM_PARALLEL_LINK_JOBS=2 -DLLVM_USE_LINKER=gold \
       -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      ../llvm-project/llvm
+      -Wno-dev ../llvm-project/llvm
 
 cmake --build . --target install
 popd
 
-mkdir -p build-newlib
-pushd build-newlib
-CC_FOR_TARGET=clang \
-CFLAGS_FOR_TARGET=" --target=kvx-osp --sysroot=$PREFIX" \
-AS_FOR_TARGET=${TARGET}-as \
-LD_FOR_TARGET=${TARGET}-ld \
-RANLIB_FOR_TARGET=${TARGET}-ranlib \
-AR_FOR_TARGET=${TARGET}-ar \
-    ../newlib/configure \
-    --with-sysroot="$PREFIX" \
-    --target=kvx-llvmosp \
-    --prefix="$PREFIX" \
-    --enable-multilib \
-    --enable-target-optspace=no \
-    --enable-initfini-array \
-    --enable-newlib-io-c99-formats \
-    --enable-newlib-multithread
-
-make all "$PARALLEL_JOBS" > /dev/null
-make install
-popd
-
-mkdir -p build-crt
-pushd build-crt
-# TODO
-popd
-
 echo "Finished"
-
